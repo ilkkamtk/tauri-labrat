36adb8ca143553b3d1476c08a6f891ea3c8a911f
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-01-20 10:47:13 +0200

    implement database operations for faces and votes in useDB hook

--- src/hooks/DBHooks.ts
+++ src/hooks/DBHooks.ts
@@ -24,9 +24,56 @@ const useDB = () => {
         const votes =
           dbInstance.getCollection<Vote>('votes') ||
           dbInstance.addCollection('votes');
+        setDB(dbInstance);
+        setFaceCollection(faces);
+        setVoteCollection(votes);
       });
     } catch (error) {
       console.error('useDB error', error);
     }
   }, []);
+
+  const getAllFaces = () => {
+    if (!faceCollection) {
+      return [];
+    }
+    return faceCollection.find();
+  };
+
+  const getAllVotes = () => {
+    if (!voteCollection) {
+      return [];
+    }
+    return voteCollection.find();
+  };
+
+  const addFaces = (face: Float32Array) => {
+    if (!db || !faceCollection) {
+      throw new Error('No database or collection');
+    }
+    const response = faceCollection.insert(face);
+    db.saveDatabase();
+    return response;
+  };
+
+  const addVotes = (vote: Vote) => {
+    if (!db || !voteCollection) {
+      throw new Error('No database or collection');
+    }
+    const response = voteCollection.insert(vote);
+    db.saveDatabase();
+    return response;
+  };
+
+  const deleteAllFromDB = () => {
+    if (!db || !voteCollection || !faceCollection) {
+      throw new Error('No database or collection');
+    }
+    faceCollection.clear();
+    voteCollection.clear();
+    db.saveDatabase();
+  };
+  return { addFaces, addVotes, getAllFaces, getAllVotes, deleteAllFromDB };
 };
+
+export { useDB };
