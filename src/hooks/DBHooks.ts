4b493334147a8de5036cf6d388e82bc237598fe7
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-01-20 10:13:05 +0200

    add lokijs integration for database management and define Vote type

--- src/hooks/DBHooks.ts
+++ src/hooks/DBHooks.ts
@@ -1,1 +1,32 @@
-const useDB = () => {};
+import { useEffect, useState } from 'react';
+import Loki from 'lokijs';
+import { Vote } from '@/types/localTypes';
+
+const useDB = () => {
+  const [db, setDB] = useState<Loki | null>(null);
+  const [faceCollection, setFaceCollection] =
+    useState<Loki.Collection<Float32Array> | null>(null);
+  const [voteCollection, setVoteCollection] =
+    useState<Loki.Collection<Vote> | null>(null);
+
+  useEffect(() => {
+    try {
+      const dbInstance = new Loki('1.json', {
+        persistenceMethod: 'fs',
+      });
+
+      // load database if it exists
+      dbInstance.loadDatabase({}, () => {
+        // get or create collection of documents
+        const faces =
+          dbInstance.getCollection<Float32Array>('faces') ||
+          dbInstance.addCollection('faces');
+        const votes =
+          dbInstance.getCollection<Vote>('votes') ||
+          dbInstance.addCollection('votes');
+      });
+    } catch (error) {
+      console.error('useDB error', error);
+    }
+  }, []);
+};
