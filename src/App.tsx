38a7c0a51f0c71d09a0e8bb16fccc36223c83fe4
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-01-27 09:51:29 +0200

    refactor database context handling by removing DbProvider and integrating Zustand for state management

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,18 @@ import Layout from './views/Layout';
 import Home from './views/Home';
 import DetectFace from './views/DetectFace';
 import Detected from './views/Detected';
-import { DbProvider } from './contexts/DBContext';
 
 const App = () => {
   return (
-    <DbProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/face" element={<DetectFace />} />
-            <Route path="/detected" element={<Detected />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </DbProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/face" element={<DetectFace />} />
+          <Route path="/detected" element={<Detected />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/stores/DBStore.ts
@@ -0,0 +1,67 @@
+import Loki from 'lokijs';
+import { Vote } from '@/types/localTypes';
+import { create } from 'zustand';
+
+type DBState = {
+  db: Loki | null;
+  facesCollection: Collection<Float32Array> | null;
+  votesCollection: Collection<Vote> | null;
+  isReady: boolean;
+  faces: (Float32Array & LokiObj)[];
+  votes: (Vote & LokiObj)[];
+};
+
+type DBStore = DBState & {
+  init: () => Promise<void>;
+  addFaces: (face: Float32Array) => Float32Array | undefined;
+  addVotes: (vote: Vote) => Vote | undefined;
+  deleteAllFromDB: () => void;
+};
+
+export const useStore = create<DBStore>((set, get) => ({
+  db: null,
+  facesCollection: null,
+  votesCollection: null,
+  isReady: false,
+  faces: [],
+  votes: [],
+
+  init: () => {
+    return new Promise((resolve, reject) => {
+      const db = new Loki('1.json', {
+        autoload: true,
+        autoloadCallback: () => {
+          const facesCollection =
+            db.getCollection('faces') || db.addCollection('faces');
+          const votesCollection =
+            db.getCollection('votes') || db.addCollection('votes');
+          set({
+            db,
+            facesCollection,
+            votesCollection,
+            isReady: true,
+            faces: facesCollection.find(),
+            votes: votesCollection.find(),
+          });
+          resolve();
+        },
+        autosave: true,
+      });
+    });
+  },
+  addFaces: (face) => {
+    const result = get().facesCollection?.insert(face);
+    set({ faces: get().facesCollection?.find() || [] });
+    return result;
+  },
+  addVotes: (vote) => {
+    const result = get().votesCollection?.insert(vote);
+    set({ votes: get().votesCollection?.find() || [] });
+    return result;
+  },
+  deleteAllFromDB: () => {
+    get().facesCollection?.clear();
+    get().votesCollection?.clear();
+    set({ faces: [], votes: [] });
+  },
+}));
